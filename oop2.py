6f97bca31f42f682feb04048db7f1ee2eab121db
coherent17 <mnb51817@gmail.com>
2021-02-24 21:10:02 +0800

    test

--- oop2.py
+++ oop2.py
@@ -10,9 +10,9 @@ class Pet:
         print("I don't know what I say")
 #inherit
 class Cat(Pet):
+    #initialize again in the lower class
     def __init__(self,name,age,color):
-        self.name=name
-        self.age=age
+        super().__init__(name,age)
         self.color=color
 
     #because the lower class has the method with same name as the higher
@@ -20,34 +20,31 @@ class Cat(Pet):
     def speak(self):
         print("Meow")
 
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and I am {self.color}")
+
+
 class Dog(Pet):
     def speak(self):
         print("Bark")
 
+
 class Fish(Pet):
+    #if no defined the method then it will use the higher class method
     pass
 
 p=Pet("Tim",19)
 p.show()
-#I am Tim and I am 19 years old
 p.speak()
-#I don't know what I say
 
-c=Cat("Bill",34)
+c=Cat("Bill",34,"red")
 c.show()
-#I am Bill and I am 34 years old
 c.speak()
-#I am Bill and I am 34 years old
-#Meow
 
 d=Dog("Jill",25)
 d.show()
-#I am Jill and I am 25 years old
 d.speak()
-#Bark
 
 f=Fish("Bubbles",10)
 f.speak()
-#I don't know what I say  
-#because there is no speak from the class Fish so it use the
-#higher class method
+
