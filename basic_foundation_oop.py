9653bf83da66bc8bd93685e21081cdd29f9b2786
coherent17 <mnb51817@gmail.com>
2021-02-25 01:19:09 +0800

    test

--- basic_foundation_oop.py
+++ basic_foundation_oop.py
@@ -11,13 +11,18 @@ class Car:
     def __init__(self,color,seat):
         self.color=color #顏色屬性
         self.seat=seat #座位屬性
+    # what does "self" means?
+    #self tell class which attribute is setting
+    #the attribute 'color' of the object = input color
+    
 
-    #方法
+    #方法 need at least one self parameter
     def drive(self):
         print(f"My car is {self.color} and {self.seat} seats")
 
 #Mazda是Car類別中的物件
 Mazda=Car("red",4)
+Mazda.drive() #My car is red and 4 seats
 
 #use isinstance to judge the relationship between the class and the object
 print(isinstance(Mazda,Car)) #True
